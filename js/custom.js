5d63e6db0f6a3f6a8e4727f725226c42b3b0c4ef
Diego Pino Navarro <dpino@metro.org>
2023-05-12 20:01:37 -0400

    I want popovers!

--- js/custom.js
+++ js/custom.js
@@ -10,9 +10,14 @@
   Drupal.behaviors.archipelago_subtheme_rpi = {
     attach: function (context, settings) {
       if ($(context).is('.view') || context == document) {
-          $("#main-breadcrumbs").find('.views-display-link').remove();
-          $(context).once('view-header-rpi').find('.view-header .views-display-link').each(function () {
-            $(this).detach().appendTo("#main-breadcrumbs");
+        /* Initialize Popovers */
+        var popoverTriggerList = [].slice.call(context.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+          return new bootstrap.Popover(popoverTriggerEl)
+        })
+        $("#main-breadcrumbs").find('.views-display-link').remove();
+        $(context).once('view-header-rpi').find('.view-header .views-display-link').each(function () {
+          $(this).detach().appendTo("#main-breadcrumbs");
         });
       }
     }
